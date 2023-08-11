76877ea94c05fb87fdf2cc7ad250db9f847ed4b3
skycloudd <51929172+skycloudd@users.noreply.github.com>
2023-08-11 02:09:45 +0200

    add endgame king pst

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -7,6 +7,8 @@ pub fn evaluate_position(board: &Board) -> i32 {
         chess::BoardStatus::Ongoing => {
             let mut score = 0;
 
+            let is_endgame = is_endgame(board);
+
             for sq in 0..64 {
                 let square = unsafe { Square::new(sq) }; // safety: square is always 0..=63
 
@@ -20,7 +22,7 @@ pub fn evaluate_position(board: &Board) -> i32 {
                         Piece::Rook => 500,
                         Piece::Queen => 900,
                         Piece::King => 20000,
-                    } + piece_square(&piece, piece_colour, square);
+                    } + piece_square(&piece, piece_colour, square, is_endgame);
 
                     score += match piece_colour {
                         Color::White => piece_score,
@@ -44,14 +46,20 @@ pub fn evaluate_position(board: &Board) -> i32 {
     }
 }
 
-fn piece_square(piece: &Piece, piece_colour: Color, square: Square) -> i32 {
+fn piece_square(piece: &Piece, piece_colour: Color, square: Square, is_endgame: bool) -> i32 {
     let table = match piece {
         Piece::Pawn => PAWN_TABLE,
         Piece::Knight => KNIGHT_TABLE,
         Piece::Bishop => BISHOP_TABLE,
         Piece::Rook => ROOK_TABLE,
         Piece::Queen => QUEEN_TABLE,
-        Piece::King => KING_TABLE,
+        Piece::King => {
+            if is_endgame {
+                KING_TABLE_ENDGAME
+            } else {
+                KING_TABLE
+            }
+        }
     };
 
     let index = match piece_colour {
@@ -98,3 +106,63 @@ const KING_TABLE: [i32; 64] = [
     -30, -20, -10, -20, -20, -20, -20, -20, -20, -10, 20, 20, 0, 0, 0, 0, 20, 20, 20, 30, 10, 0, 0,
     10, 30, 20,
 ];
+
+const KING_TABLE_ENDGAME: [i32; 64] = [
+    -50, -40, -30, -20, -20, -30, -40, -50, -30, -20, -10, 0, 0, -10, -20, -30, -30, -10, 20, 30,
+    30, 20, -10, -30, -30, -10, 30, 40, 40, 30, -10, -30, -30, -10, 30, 40, 40, 30, -10, -30, -30,
+    -10, 20, 30, 30, 20, -10, -30, -30, -30, 0, 0, 0, 0, -30, -30, -50, -30, -30, -30, -30, -30,
+    -30, -50,
+];
+
+fn is_endgame(board: &Board) -> bool {
+    if board.pieces(Piece::Queen).0.count_ones() == 0 {
+        true
+    } else {
+        let white_queens = (board.pieces(Piece::Queen) & board.color_combined(Color::White))
+            .0
+            .count_ones();
+        let black_queens = (board.pieces(Piece::Queen) & board.color_combined(Color::Black))
+            .0
+            .count_ones();
+
+        let white_endgame = if white_queens == 1 {
+            let white_minor_pieces = ((board.pieces(Piece::Knight) | board.pieces(Piece::Bishop))
+                & board.color_combined(Color::White))
+            .0
+            .count_ones();
+
+            let white_rooks = (board.pieces(Piece::Rook) & board.color_combined(Color::White))
+                .0
+                .count_ones();
+
+            if white_minor_pieces <= 1 && white_rooks == 0 {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        let black_endgame = if black_queens == 1 {
+            let black_minor_pieces = ((board.pieces(Piece::Knight) | board.pieces(Piece::Bishop))
+                & board.color_combined(Color::Black))
+            .0
+            .count_ones();
+
+            let black_rooks = (board.pieces(Piece::Rook) & board.color_combined(Color::Black))
+                .0
+                .count_ones();
+
+            if black_minor_pieces <= 1 && black_rooks == 0 {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        white_endgame && black_endgame
+    }
+}
